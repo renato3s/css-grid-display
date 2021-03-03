6e26cab3fcb9677dd723cd04a26331828b9f9d46
Renato <renatomurilosil@protonmail.com>
2021-03-03 10:33:20 -0300

    upload grid sample

---
+++ src/components/Container.tsx
@@ -0,0 +1,46 @@
+export default function Container() {
+    return (
+        <div className="container">
+            <div className="header">
+                <h1>footer</h1>
+            </div>
+            <div className="wrapper">
+                <div className="option">
+                    <div className="optionHeader">
+                        Name
+                    </div>
+                    <div className="optionContent">
+                        Content
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="optionHeader">
+                        Name
+                    </div>
+                    <div className="optionContent">
+                        Content
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="optionHeader">
+                        Name
+                    </div>
+                    <div className="optionContent">
+                        Content
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="optionHeader">
+                        Name
+                    </div>
+                    <div className="optionContent">
+                        Content
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <h1>footer</h1>
+            </div>
+        </div>
+    )
+}
